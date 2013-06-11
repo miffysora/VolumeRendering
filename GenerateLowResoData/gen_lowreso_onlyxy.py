bf57243de4bd0cab74996be3c06b064c77d13d82
jasmingirl <encomium_moriae@hotmail.com>
2013-06-11 16:05:02 +0900

    Update gen_lowreso_onlyxy.py
    xy平面のリサイズにPILを使って、for文がなくなりました

--- GenerateLowResoData/gen_lowreso_onlyxy.py
+++ GenerateLowResoData/gen_lowreso_onlyxy.py
@@ -4,15 +4,13 @@ from PIL import Image
 def GeneratLowResoData(_highsize,_constz,_data):#xy方向だけ圧縮
     lowsize=_highsize>>1;
     for z in range(0,_constz):
-        for y in range(0,lowsize):
-            for x in range(0,lowsize):
-                sumval=0.0;
-                for inner_y in range(y*2,y*2+2):
-                    for inner_x in range(x*2,x*2+2):
-                            sumval+=float(_data[(z*_highsize+inner_y)*_highsize+inner_x])/4.0;
-                _data[(z*lowsize+y)*lowsize+x]=sumval;
+        zslice=Image.new('L',(_highsize,_highsize));
+        start=_highsize*_highsize*z;
+        zslice.putdata(_data[start:start+_highsize*_highsize]);
+        lowered=zslice.resize((lowsize,lowsize));
+        _data[start:start+_highsize*_highsize]=lowered.getdata();
     return lowsize;
-#data=numpy.zeros((360,200));#360×200 で0で埋めつくされた配列の宣言
+
 filename="DBZ201208301319_512_z64_2byte.dat"
 splitted=filename.split('_');
 
@@ -23,7 +21,7 @@ fp=open(filename,"rb");
 data=numpy.fromfile(fp,dtype='H');
 print("最小値%d,最大値%d"%(numpy.amin(data),numpy.amax(data)));
 print("サイズ%d"%len(data));
-fp.close();  			
+fp.close();    			
 im=Image.new( 'L', ( xynum, xynum ) );
 slicez=0;
 maxval=numpy.amax(data)
@@ -36,7 +34,7 @@ im.save(dataname+".png");
 lowsize=GeneratLowResoData(xynum,znum,uchardata);
 print("lowsize=%d"%lowsize);
 fp=open(dataname+"_"+str(lowsize)+"_z"+str(znum)+"_1byte.raw","wb");
-uchardata.tofile(fp);
+uchardata[:lowsize*lowsize*znum].tofile(fp);
 fp.close();
 im=Image.new( 'L', (lowsize, lowsize) );
 im.putdata(uchardata[:lowsize*lowsize]);
