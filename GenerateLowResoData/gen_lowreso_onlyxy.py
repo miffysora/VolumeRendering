7995ab0f22a6e09c6ba862f520dfb17807f2144f
jasmingirl <encomium_moriae@hotmail.com>
2013-06-11 16:15:20 +0900

    Update gen_lowreso_onlyxy.py

--- GenerateLowResoData/gen_lowreso_onlyxy.py
+++ GenerateLowResoData/gen_lowreso_onlyxy.py
@@ -1,14 +1,19 @@
 # -*- coding: cp932 -*-
 import numpy;
 from PIL import Image
-def GeneratLowResoData(_highsize,_constz,_data):#xy方向だけ圧縮
+def GeneratLowResoData(_highsize,_constz,_data,_dataname):#xy方向だけ圧縮
     lowsize=_highsize>>1;
+    print("lowsize%d"%lowsize)
     for z in range(0,_constz):
         zslice=Image.new('L',(_highsize,_highsize));
         start=_highsize*_highsize*z;
         zslice.putdata(_data[start:start+_highsize*_highsize]);
         lowered=zslice.resize((lowsize,lowsize));
-        _data[start:start+_highsize*_highsize]=lowered.getdata();
+        lowstart=lowsize*lowsize*z;
+        _data[lowstart:lowstart+lowsize*lowsize]=lowered.getdata();
+    fp=open(_dataname+"_"+str(lowsize)+"_z"+str(_constz)+"_1byte.raw","wb");
+    _data[:lowsize*lowsize*znum].tofile(fp);
+    fp.close();
     return lowsize;
 
 filename="DBZ201208301319_512_z64_2byte.dat"
@@ -29,15 +34,9 @@ maxval=numpy.amax(data)
 fdata=numpy.array(data,dtype='f')
 fdata=fdata/numpy.amax(fdata)*255.0;#0-255に正規化
 uchardata=numpy.array(fdata,dtype='B');
-im.putdata(uchardata[:xynum*xynum]);
-im.save(dataname+".png");
-lowsize=GeneratLowResoData(xynum,znum,uchardata);
-print("lowsize=%d"%lowsize);
-fp=open(dataname+"_"+str(lowsize)+"_z"+str(znum)+"_1byte.raw","wb");
-uchardata[:lowsize*lowsize*znum].tofile(fp);
-fp.close();
-im=Image.new( 'L', (lowsize, lowsize) );
-im.putdata(uchardata[:lowsize*lowsize]);
-im.save(dataname+"_"+str(lowsize)+".png");
-print("done");
+lowsize=xynum;
+while lowsize>32:
+    lowsize=GeneratLowResoData(lowsize,znum,uchardata,dataname);
+
+
 
